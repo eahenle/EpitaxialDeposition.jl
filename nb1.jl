147dd73a24815196fbb0caaf4947c8b49e480f50
Adrian Henle <eahenle@gmail.com>
2022-02-13 10:33:36 -0800

    example commit

---
+++ nb1.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.17.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ baabdc02-8cf9-11ec-24cc-f3327dea8ff8
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.1"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═baabdc02-8cf9-11ec-24cc-f3327dea8ff8
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
