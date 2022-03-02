a7e7ce5bd2e479a1491d9cbcc6e79d46140c538d
eahenle <eahenle@gmail.com>
2022-03-01 20:41:18 -0800

    packaged and optimized

---
+++ src/reaction_model.jl
@@ -0,0 +1,22 @@
+# define the temperature-dependent equilibrium constant expressions
+K(T::Float64)::Float64   = PARAMS[:rate][:K0]  * exp(-PARAMS[:energy][:E]   / (PARAMS[:R][:kcal_mol_K] * T)) # [cm/s]
+K2(T::Float64)::Float64  = PARAMS[:rate][:K20] * exp(-PARAMS[:energy][:Ea2] / (PARAMS[:R][:kcal_mol_K] * T)) # [cm/s]
+K3(T::Float64)::Float64  = PARAMS[:rate][:K30] * exp(-PARAMS[:energy][:Ea3] / (PARAMS[:R][:kcal_mol_K] * T)) # [cm/s]
+kKp(T::Float64)::Float64 = PARAMS[:kKp][:k] * 10^(PARAMS[:kKp][:Ea] - PARAMS[:kKp][:b] / T) # [∅]
+
+# register the equilibrium constant expressions with Catalyst
+@register_symbolic kKp(T)
+@register_symbolic K(T)
+@register_symbolic K2(T) 
+@register_symbolic K3(T)
+
+# define the reaction network for the deposition process with competitive etching
+deposition_rxn_network = @reaction_network begin
+    K(T), SiCl₄ + 2H₂ --> Si_dep + 4HCl     # Si deposition from source gas
+    K2(T), 2HCl --> SiCl₂ + H₂ + Si_etch    # Si etching by HCl
+    kKp(T), SiCl₄ --> 2SiCl₂ + Si_etch      # Si etching by SiCl₄
+    k, 2SiCl₂ --> SiCl₄ + Si_dep            # Si deposition by SiCl₂ decomposition
+end
+
+# generate the system of ODEs
+odesys = convert(ODESystem, deposition_rxn_network, combinatoric_ratelaws=false)
