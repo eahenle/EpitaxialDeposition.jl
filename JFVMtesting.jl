5198b404b94ffad78a9f97dc5ca3bf959c93f73c
Ian Harreschou <ianharreschou@gmail.com>
2022-03-08 11:33:42 -0800

    most recent updates

--- JFVMtesting.jl
+++ JFVMtesting.jl
@@ -9,12 +9,12 @@ begin
 	import Pkg; using Pkg
 	Pkg.add(url="https://github.com/simulkade/JFVMvis.jl")
 	Pkg.add(url="https://github.com/simulkade/JFVM.jl")
-	Pkg.add("PyPlot")
-	Pkg.add("CairoMakie")
+	Pkg.add(["Dierckx", "CairoMakie", "PyPlot", "LsqFit"])
+	
 end
 
 # ╔═╡ d2eb8940-9b16-11ec-1d74-3bc00f5f950f
-using JFVM, JFVMvis, CairoMakie
+using JFVM, JFVMvis, CairoMakie, Dierckx, LsqFit
 
 # ╔═╡ 6e4302dc-b2ce-4d8f-809b-078f2c39c2dc
 md"""
@@ -52,7 +52,7 @@ Values for σ and a parameter ϵ are tabulated in the book referenced, Appendix 
 # ╔═╡ e137888b-fc7e-4ddb-b25e-3c53bb8ee23a
 begin
 	# random system parameters and necessary constants
-	const k = 1.38e-16 # [ergs/K]
+	const κ = 1.38e-16 # [ergs/K]
 	const T = 1200 # [K]
 	const P = 1 # [atm]
 end
@@ -81,7 +81,7 @@ function interpolate_kTϵ!(x_1, y_1, x_2, y_2, x_target)
 end
 
 # ╔═╡ cb705cc5-23d3-4476-99e0-9542572b9e25
-interpolate_Ω!(1.128, 1.75, 1.116, 1.80, 1.84)
+interpolate_Ω!(1.128, 1.75, 1.116, 1.80, 1.76)
 
 # ╔═╡ 41e99faa-bba3-4bd2-8456-78a4c3e0b048
 interpolate_kTϵ!(1.128, 1.75, 1.116, 1.80, 1.1064)
@@ -90,10 +90,125 @@ interpolate_kTϵ!(1.128, 1.75, 1.116, 1.80, 1.1064)
 begin
 	MW_dict = Dict(:SiCl₄ => 169.9, :H₂ => 2.0, :HCl => 36.5, :SiCl2 => 99) # [g/gmol]
 	σ_dict = Dict(:SiCl₄ => 5.08, :H₂ => 2.968, :HCl => 3.305) # [Å]
-	ϵ_dict = Dict(:SiCl₄ => 358*k, :H₂ => 33.3*k, :HCl => 360*k) # [ergs]
+	ϵ_dict = Dict(:SiCl₄ => 358*κ, :H₂ => 33.3*κ, :HCl => 360*κ) # [ergs]
 	ν_dict = Dict(:HCl => [19.5, 1.98], :H₂ => 7.07)
+	Tc_dict = Dict(:HCl => 324.68, :SiCl₄ => 508.1, :H₂ => 33.18) # [K]
+	Pc_dict = Dict(:HCl => 82.56 / 1.013, :SiCl₄ => 35.93 / 1.013, :H₂ => 13.00 / 1.013) # [atm]
 
-	Params = Dict(:MW => MW_dict, :σ => σ_dict, :ϵ => ϵ_dict, :ν => ν_dict) # store all data in a single dictionary
+	Params = Dict(:MW => MW_dict, 
+				  :σ => σ_dict, 
+				  :ϵ => ϵ_dict, 
+				  :ν => ν_dict,
+				  :Pc => Pc_dict,
+				  :Tc => Tc_dict) # store all data in a single dictionary
+end
+
+# ╔═╡ 1333c9f1-6202-4df9-9b09-4d19f2b91d65
+begin
+	# load in Lennard-Jones Constants (App. K, Welty et al.)
+	a = [0.30 2.785 2.662 1.80 1.221 1.116
+	0.35 2.628 2.476 1.85 1.209 1.105
+	0.40 2.492 2.318 1.90 1.197 1.094
+	0.45 2.368 2.184 1.95 1.186 1.084
+	0.50 2.257 2.066 2.00 1.175 1.075
+	0.55 2.156 1.966 2.10 1.156 1.057
+	0.60 2.065 1.877 2.20 1.138 1.041
+	0.65 1.982 1.798 2.30 1.122 1.026
+	0.70 1.908 1.729 2.40 1.107 1.012
+	0.75 1.841 1.667 2.50 1.093 0.9996
+	0.80 1.780 1.612 2.60 1.081 0.9878
+	0.85 1.725 1.562 2.70 1.069 0.9770
+	0.90 1.675 1.517 2.80 1.058 0.9672
+	0.95 1.629 1.476 2.90 1.048 0.9576
+	1.00 1.587 1.439 3.00 1.039 0.9490
+	1.05 1.549 1.406 3.10 1.030 0.9406
+	1.10 1.514 1.375 3.20 1.022 0.9328
+	1.15 1.482 1.346 3.30 1.014 0.9256
+	1.20 1.452 1.320 3.40 1.007 0.9186
+	1.25 1.424 1.296 3.50 0.9999 0.9120
+	1.30 1.399 1.273 3.60 0.9932 0.9058
+	1.35 1.375 1.253 3.70 0.9870 0.8998
+	1.40 1.353 1.233 3.80 0.9811 0.8942
+	1.45 1.333 1.215 3.90 0.9755 0.8888
+	1.50 1.314 1.198 4.00 0.9700 0.8836
+	1.55 1.296 1.182 4.10 0.9649 0.8788
+	1.60 1.279 1.167 4.20 0.9600 0.8740
+	1.65 1.264 1.153 4.30 0.9553 0.8694
+	1.70 1.248 1.140 4.40 0.9507 0.8652
+	4.50 0.9464 0.8610 10.0 0.8242 0.7424
+4.60 0.9422 0.8568 20.0 0.7432 0.6640
+4.70 0.9382 0.8530 30.0 0.7005 0.6232
+4.80 0.9343 0.8492 40.0 0.6718 0.5960
+4.90 0.9305 0.8456 50.0 0.6504 0.5756
+5.0 0.9269 0.8422 60.0 0.6335 0.5596
+6.0 0.8963 0.8124 70.0 0.6194 0.5464
+7.0 0.8727 0.7896 80.0 0.6076 0.5352
+8.0 0.8538 0.7712 90.0 0.5973 0.5256]
+
+	
+	b = vcat(a[:, [1, 3]], a[:, [4,6]])
+end
+
+# ╔═╡ 989cc1b3-9026-4f95-a4e7-d021188d23a8
+sp = sortperm(b[:, 1])
+
+# ╔═╡ 2f7b245d-f9e0-443d-a16e-53c18f882134
+xdata = b[:, 1][sp][60:end, 1]
+
+# ╔═╡ 0b2e555a-84f8-40c2-9e94-6ca5f393073b
+ydata = b[:, 2][sp][60:end, 1]
+
+# ╔═╡ fb36836c-bc8c-4a5d-812b-5e8eceaed845
+begin
+
+	# @. model(x,p) = p[1]*log(x*p[2])^(-1) # inverse log model
+	# @. model(x,p) = p[1]*x^(-2) + p[2] # quadratic model
+	@. model(x,p) = p[1] / (x*p[2] + p[3]) + p[4] # inverse x model
+
+	# p0 = [0.25, 0.75]
+	p0 = [0.25, 0.75, 0.5, 0.5]
+
+	fit = curve_fit(model, xdata, ydata, p0)
+
+end
+
+# ╔═╡ 6e4c3904-6543-41e9-a862-22cb749ed178
+begin
+
+	xs = range(xdata[1], xdata[end], length = 150)
+	
+	fig = Figure()
+	ax = Axis(fig[1,1],
+		ylabel = "Collision Integral Ω",
+		xlabel = "κT/ϵ")
+
+	scatter!(xdata,ydata)
+	lines!(xs, model(xs, fit.param), color = :blue)
+
+	fig
+
+end
+
+# ╔═╡ e73e4dfb-63fa-406f-9eb3-dbe1e156fb6b
+modelfit(x) = model(x, fit.param)
+
+# ╔═╡ c26da7bb-b415-4d74-9dd2-89c675724874
+function DAB(species1::Symbol, species2::Symbol, T, P)
+
+	# calculate the collision diameter from critical parameters
+	σ1 = 2.44*(Params[:Tc][species1] / Params[:Pc][species1])^(1/3)
+	σ2 = 2.44*(Params[:Tc][species2] / Params[:Pc][species2])^(1/3)
+
+	# calculate the ϵ's
+	ϵ1 = 0.77 * κ * Params[:Tc][species1]
+	ϵ2 = 0.77 * κ * Params[:Tc][species2]
+
+	# combined parameters for calculating the diffusion coefficient
+	σ12 = (σ1 + σ2) / 2
+	ϵ12 = sqrt(ϵ1 * ϵ2)
+
+	
+
 end
 
 # ╔═╡ f73c1d88-1605-4445-9c9c-ddc16937cc48
@@ -258,8 +373,16 @@ fig2
 # ╠═cb705cc5-23d3-4476-99e0-9542572b9e25
 # ╠═41e99faa-bba3-4bd2-8456-78a4c3e0b048
 # ╠═457a34ad-8514-4c6a-b16b-c94e6515ba40
+# ╠═1333c9f1-6202-4df9-9b09-4d19f2b91d65
+# ╠═989cc1b3-9026-4f95-a4e7-d021188d23a8
+# ╠═2f7b245d-f9e0-443d-a16e-53c18f882134
+# ╠═0b2e555a-84f8-40c2-9e94-6ca5f393073b
+# ╠═6e4c3904-6543-41e9-a862-22cb749ed178
+# ╠═e73e4dfb-63fa-406f-9eb3-dbe1e156fb6b
+# ╠═fb36836c-bc8c-4a5d-812b-5e8eceaed845
+# ╠═c26da7bb-b415-4d74-9dd2-89c675724874
 # ╠═f73c1d88-1605-4445-9c9c-ddc16937cc48
-# ╠═d0374368-0283-40f1-a55c-133eb1cff7e8
+# ╟─d0374368-0283-40f1-a55c-133eb1cff7e8
 # ╠═c79cb0cb-10dd-414f-89f0-f5866fd5c72b
 # ╠═98bc75ac-98c6-43aa-9753-cd960107b44b
 # ╠═7c259d4c-122c-4270-ad64-185b09be4a2f
