494a416ab3bdc1436d3e8ed1b41482a240bca3b5
eahenle <eahenle@gmail.com>
2022-03-09 00:13:08 -0800

    pass tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,15 @@
 using EpitaxialDeposition, Test
 import Aqua
 
-@testset "simulation notebook runs" begin
-    include("../simulations.jl")
-    @test true
-end
-
 Aqua.test_ambiguities(EpitaxialDeposition, recursive=false) ##! `recursive=false` b/c Aqua finds an ambiguity IN ITSELF!
 Aqua.test_unbound_args(EpitaxialDeposition)
 #Aqua.test_undefined_exports(EpitaxialDeposition) ##! name clashes in deps
 Aqua.test_project_extras(EpitaxialDeposition)
-Aqua.test_stale_deps(EpitaxialDeposition)
+Aqua.test_stale_deps(EpitaxialDeposition, ignore=[:LsqFit, :JFVM])
 Aqua.test_deps_compat(EpitaxialDeposition)
 Aqua.test_project_toml_formatting(EpitaxialDeposition)
+
+@testset "simulation notebook runs" begin
+    include("../simulations.jl")
+    @test true
+end
