1e2890b7fb820b2d48d64445abff60424913ba54
eahenle <eahenle@gmail.com>
2022-03-09 21:51:25 -0800

    almost working

--- coupled.jl
+++ coupled.jl
@@ -85,11 +85,79 @@ end
 # ╔═╡ c1d044da-26df-4eb4-96fa-a16349a2d396
 # run a Catalyst sim with call-outs to JFVM
 
+# ╔═╡ 609e3a37-5404-4fc7-833d-8e2c934bca12
+c = ones(Nx + 2, Ny + 2)
+
+# ╔═╡ f00a16d4-9fbd-4ef8-aa0f-93476443c372
+function transport_step(m::MeshStructure, D::Float64, c₀::Matrix{Float64}, Nx::Int; 
+		N_steps=5, Lx=30., Ly=30.)
+    # Define boundary conditions
+    BC = createBC(m)
+
+    BC.left.a[:] .= 1.0 
+    BC.right.a[:] .= 1.0
+
+    BC.left.b[:] .= 0.0
+    BC.right.b[:] .= 0.0
+
+    BC.left.c[:] .= 0.0 
+    BC.right.c[:] .= 0.0
+
+    # top and bottom boundary conditions for each coefficient
+    BC.top.a[1,:] .= 1.0 
+    BC.bottom.a[1,:] .= 1.0
+
+    BC.top.b[1,:] .= 0.0 
+    BC.bottom.b[1,:] .= 0.0
+
+    BC.top.c[1,:] .= 0.0 
+    BC.bottom.c[1,:] .= 0.0
+
+    BC.bottom.c[EpitaxialDeposition.wafer_boundary(Nx, Lx, EpitaxialDeposition.PARAMS[:reactor][:wafer_diameter]), 1] .= -1.0
+
+    # Give a value for the diffusion coefficient based on current system
+    D_cell = createCellVariable(m, D) # assign the diffusion coefficient as a variable to each cell of the mesh
+    D_face = geometricMean(D_cell) # choose an averaging scheme, for how the diffusion coefficient on a cell face is calculated
+    
+    c = createCellVariable(m, c₀, BC) # assign the "initial" species concentration to cells [mol/L]
+    
+    # Discretize the problem and build our solution
+    M_diff = diffusionTerm(D_face) # matrix of diffusion term coefficients
+    (M_bc, RHS_bc) = boundaryConditionTerm(BC) # matrix composed of coefficients and the right hand side for the BC
+
+    ### modify to be 1/5th of the timestep from Catalyst
+    
+    if Lx < Ly # calculate a time step based on the smaller length (for more resolution)
+    Δt = sqrt(Lx^2 / D) / N_steps # recommended time step calculation
+
+    else
+    Δt = sqrt(Ly^2 / D) / N_steps
+
+    end
+
+    # this code seems to be for taking discrete time steps forward, with size of Δt
+
+    ### parameterize the # of steps?
+    
+    for i = 1:N_steps
+        (M_t, RHS_t) = transientTerm(c, Δt, 1.0)
+        M = M_t - M_diff + M_bc # adding together all sparse matrices of coefficients
+        RHS = RHS_bc + RHS_t # add all RHS's to each other
+        c = solveLinearPDE(m, M, RHS)
+    end
+
+return c
+end
+
+# ╔═╡ 01944257-912a-49c3-a8eb-cfa25a7627ee
+transport_step(mesh, D[:H₂], c, Nx)
+
 # ╔═╡ 2c89c6df-1e94-43b2-9624-280463aaf2b9
-function f()
+function coupled_simulation()
 	kinetics = []
 	transport = []
 	u = u₀
+	c = Dict([key => ones(Nx + 2, Ny + 2) * value for (key, value) in c₀])
 
 	colnames = Symbol.(String.([u[1] for u in u₀]) .* "(t)")
 	sol_to_u = Dict([name => Symbol(chop(String(name), tail=3)) for name in colnames])
@@ -105,8 +173,11 @@ function f()
 		# append the kinetics data
 		push!(kinetics, k_sol)
 
-		# run the transport model
-		t_sol = Dict([key => trans_diff_Neumann(mesh, value, c₀[key], Nx) for (key, value) in D])
+		# run the transport models
+		t_sol = Dict([key => transport_step(mesh, value, c[key], Nx) for (key, value) in D])
+
+		# update the transport models' state for next iteration
+		c = Dict([species => cell.value[2:end-1, 2:end-1] for (species, cell) in t_sol])
 
 		# append the transport data
 		push!(transport, t_sol)
@@ -122,7 +193,7 @@ function f()
 end
 
 # ╔═╡ 0c83de9c-9f4e-4f3c-97e1-efe01fecc4a3
-kinetics, transport, δ = f();
+kinetics, transport, δ = coupled_simulation();
 
 # ╔═╡ 48063896-50eb-4e95-96a8-c563e8baaf8c
 # visualize the results
@@ -164,7 +235,7 @@ end
 ## THIS IS PICKING TIMESTEP, NOT ACTUAL TIME
 
 md"""
-Profile snapshot time: $(@bind t_i PlutoUI.Slider(0:Δt:t_max, default=0, show_value=true)) s
+Profile snapshot: $(@bind t_i PlutoUI.NumberField(0:length(transport)-1, default=0))
 """
 
 # ╔═╡ 600ebffc-d978-4813-9d06-67208b11a61d
@@ -179,6 +250,9 @@ end
 # ╠═4411f086-c12c-45c4-a5b0-bd6d9ac2bcd9
 # ╠═54e026fa-12c1-4906-b96e-88b11eabfb38
 # ╠═c1d044da-26df-4eb4-96fa-a16349a2d396
+# ╠═609e3a37-5404-4fc7-833d-8e2c934bca12
+# ╠═01944257-912a-49c3-a8eb-cfa25a7627ee
+# ╠═f00a16d4-9fbd-4ef8-aa0f-93476443c372
 # ╠═2c89c6df-1e94-43b2-9624-280463aaf2b9
 # ╠═0c83de9c-9f4e-4f3c-97e1-efe01fecc4a3
 # ╠═48063896-50eb-4e95-96a8-c563e8baaf8c
